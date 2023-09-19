92870f02ec3faca33883d2f80bddf6c072e357c3
Mehedi5271 <mhassan201205@bscse.uiu.ac.bd>
2023-09-19 22:46:25 +0600

    program2.py

---
+++ program2.py
@@ -0,0 +1,7 @@
+name = 'hassan'
+age = 24
+
+print('our new student name is '+name)
+print(name+' lives in dhaka')
+print("she is currently", age, "years old")
+print('at the age of ', age, ' she started to lear phyhon')
