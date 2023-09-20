d001e617b3420a41e3caeefe7f09c050ee290f3c
Mehedi5271 <mhassan201205@bscse.uiu.ac.bd>
2023-09-21 00:49:43 +0600

    program8.py

---
+++ program8.py
@@ -0,0 +1,27 @@
+num1= 10
+num2 =33
+
+print(f"{num1}+{num2}= {num1+num2}")
+
+
+marks = 80
+
+
+if marks>= 33:
+    print('pass')
+if marks<33:
+    print('fail')
+
+print('-----------')
+n1 =10
+n2 =433
+n3 =37
+
+if n1>n2:
+    if n1>n3:
+        print('large number is ',n1)
+if n2>n1:
+    if n2>n3:
+        print('large number is ',n2)
+    else: print('number ',n3 ,'is large')
+
