ce13a0fa27b06efeadb81896c7a2ae953df1376b
Mehedi5271 <mhassan201205@bscse.uiu.ac.bd>
2023-09-20 17:09:19 +0600

    program2.py

---
+++ program4.py
@@ -0,0 +1,11 @@
+name = input('enter your name :')
+age = input('enter your age :')
+cgpa = input('enter your cgpa :')
+
+print('student information')
+print("---------------------")
+print('my name is '+name)
+print('my age is', age)
+print('my cgpa is '+cgpa)
+
+
