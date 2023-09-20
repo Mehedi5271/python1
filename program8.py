d455d441d4cc064197563becf1304d797e937ae5
Mehedi5271 <mhassan201205@bscse.uiu.ac.bd>
2023-09-21 02:24:55 +0600

    program8.2.py

--- program8.py
+++ program8.py
@@ -23,5 +23,16 @@ if n1>n2:
 if n2>n1:
     if n2>n3:
         print('large number is ',n2)
+
     else: print('number ',n3 ,'is large')
+
+    print('-----------')
+
+sum = 0
+i = 1
+
+while i <= 100:
+    sum = sum+i
+    i= i+1
+print(sum)
 
