8d224ff24d038711fc7ecba12d01b74e1c8817a6
Mehedi5271 <mhassan201205@bscse.uiu.ac.bd>
2023-09-21 21:57:44 +0600

    matrix

---
+++ program11.py
@@ -0,0 +1,18 @@
+name =[10,20,30,40,50]
+print(name)
+
+count =0
+n= len(name)
+
+while count<n :
+    print(name[count])
+    count =count+1
+
+print('-------------')
+
+num =[10,20,30,40,50]
+
+for x in num:
+    print(x)
+
+
