9a0e3fd27f476f2e51034c3c21c9d550e8218c45
Mehedi5271 <mhassan201205@bscse.uiu.ac.bd>
2023-09-24 08:50:19 +0600

    oop

---
+++ program16.py
@@ -0,0 +1,18 @@
+class student:
+    roll = ""
+    cgpa =""
+
+mehedi = student()
+korim = student()
+
+# print(isinstance(mehedi,student))
+
+mehedi.roll= "324"
+mehedi.cgpa= "2.3"
+print(f"roll:{mehedi.roll},Cgpa: {mehedi.cgpa} ")
+
+
+korim.roll= "44"
+korim.cgpa= "4.3"
+print(f"roll:{korim.roll},Cgpa: {korim.cgpa} ")
+
